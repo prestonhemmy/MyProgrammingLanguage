1a7dd2ddfb9f3b13e01e378a5df7113d09a296d5
prestonhemmy <phemmy@ufl.edu>
2025-03-13 16:06:14 -0400

    chore: init

--- src/main/java/plc/project/parser/Ast.java
+++ src/main/java/plc/project/parser/Ast.java
@@ -96,4 +96,46 @@ public sealed interface Ast {
 
     }
 
+    interface Visitor<T, E extends Exception> {
+
+        default T visit(Ast ast) throws E {
+            return switch (ast) {
+                case Source source -> visit(source);
+                case Stmt.Let stmt -> visit(stmt);
+                case Stmt.Def stmt -> visit(stmt);
+                case Stmt.If stmt -> visit(stmt);
+                case Stmt.For stmt -> visit(stmt);
+                case Stmt.Return stmt -> visit(stmt);
+                case Stmt.Expression stmt -> visit(stmt);
+                case Stmt.Assignment stmt -> visit(stmt);
+                case Expr.Literal expr -> visit(expr);
+                case Expr.Group expr -> visit(expr);
+                case Expr.Binary expr -> visit(expr);
+                case Expr.Variable expr -> visit(expr);
+                case Expr.Property expr -> visit(expr);
+                case Expr.Function expr -> visit(expr);
+                case Expr.Method expr -> visit(expr);
+                case Expr.ObjectExpr expr -> visit(expr);
+            };
+        }
+
+        T visit(Source ast) throws E;
+        T visit(Stmt.Let ast) throws E;
+        T visit(Stmt.Def ast) throws E;
+        T visit(Stmt.If ast) throws E;
+        T visit(Stmt.For ast) throws E;
+        T visit(Stmt.Return ast) throws E;
+        T visit(Stmt.Expression ast) throws E;
+        T visit(Stmt.Assignment ast) throws E;
+        T visit(Expr.Literal ast) throws E;
+        T visit(Expr.Group ast) throws E;
+        T visit(Expr.Binary ast) throws E;
+        T visit(Expr.Variable ast) throws E;
+        T visit(Expr.Property ast) throws E;
+        T visit(Expr.Function ast) throws E;
+        T visit(Expr.Method ast) throws E;
+        T visit(Expr.ObjectExpr ast) throws E;
+
+    }
+
 }
